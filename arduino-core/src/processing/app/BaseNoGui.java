681bb1c4f9c522552f7dffa0e1c92ca4c61aaf9b
alfran <francesco@arduino.org>
2015-07-03 12:24:23 +0200

    updated IDE info and release note for 1.7.6

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -37,9 +37,9 @@ import processing.app.packages.LibraryList;
 public class BaseNoGui {
 
   /** Version string to be used for build */
-  public static final int REVISION = 10705;
+  public static final int REVISION = 10706;
   /** Extended version string displayed on GUI */
-  static String VERSION_NAME = "1.7.5";
+  static String VERSION_NAME = "1.7.6";
 
   static File buildFolder;
 
