04cebd9d7e061d216004edc1a2be94c886d30dc8
quasto <sergio@arduino.org>
2016-02-18 09:50:46 +0100

    remove comment

--- arduino-core/src/cc/arduino/packages/DiscoveryManager.java
+++ arduino-core/src/cc/arduino/packages/DiscoveryManager.java
@@ -45,7 +45,6 @@ public class DiscoveryManager {
     discoverers = new ArrayList<Discovery>();
     discoverers.add(new SerialDiscovery());
     discoverers.add(new NetworkDiscovery());
-//TODO add new Discovery for ESP8266 ?
     // Start all discoverers
     for (Discovery d : discoverers) {
       try {
